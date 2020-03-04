9ea46af25372f9ea8053c92a05f40a997ca7a182
Roger Sentongo <rogersentongo@Rogers-MBP.home>
2020-03-03 20:02:34 -0500

    First commit, figure 4.3 in text located Chpt4 folder

---
+++ Chpt4/4-3.cc
@@ -0,0 +1,17 @@
+/*
+    Roger Sentongo March 3rd 2020.
+    Figure 4.3 of Data Structures & Algorithm Analysis in C++
+*/
+#include <iostream>
+
+//Below is the implementation of a tree
+template<class Object>
+struct TreeNode{
+
+    Object element;
+    TreeNode * firstchild;
+    TreeNode * nextSibling;
+
+
+
+}
