640046d405c295c5a29b493b83f902b7ab2f34e2
Roger Sentongo <rogersentongo@Rogers-MBP.home>
2020-03-03 20:48:22 -0500

    Added figure 4.13 on page(128 ir 147 if looking at preview numbering)

---
+++ Chpt4/4-13.cc
@@ -0,0 +1,14 @@
+/*
+    Roger Sentongo March 3rd 2020.
+    Figure 4.13 of Data Structures & Algorithm Analysis in C++
+    This is the structure of a Binary Tree
+*/
+
+template<class Object>
+struct BinaryTree{
+
+    Object element;
+    BinaryTree *leftChild;
+    BinaryTree * rightChild;
+
+}
